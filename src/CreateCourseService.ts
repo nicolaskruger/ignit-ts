35915bfa17c3d33a1421bf21986be49bb5e2b065
nicolas-kruger <nicolas.nkruger@gmail.com>
2022-01-27 14:33:29 -0300

    adicionando tipos

---
+++ src/CreateCourseService.ts
@@ -0,0 +1,14 @@
+
+interface Course {
+    name: string,
+    duration: number,
+    educator: string
+}
+
+export class CreateCourseService {
+    static execute({ name, duration, educator }: Course) {
+        console.log({ name, duration, educator });
+
+    }
+}
+
---
+++ src/routes.ts
@@ -0,0 +1,11 @@
+import { RequestHandler } from "express"
+import { CreateCourseService } from "./CreateCourseService"
+
+export const createCourse: RequestHandler = (req, res) => {
+    CreateCourseService.execute(
+        { name: "NodeJS", duration: 10, educator: "Nicolas" }
+    );
+
+    return res.send();
+
+}
