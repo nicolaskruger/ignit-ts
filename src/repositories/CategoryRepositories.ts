083897f3a0078bcce91c190af055a8c18974fa93
nicolas-kruger <nicolas.nkruger@gmail.com>
2022-01-28 16:15:40 -0300

    criando repositorio de categoria

---
+++ src/repositories/CategoryRepositories.ts
@@ -0,0 +1,21 @@
+import { Category } from '../model/Category'
+
+interface ICreateCategoryDTO {
+    name: string,
+    description: string
+}
+
+export class CategoriesRepository {
+    private categories: Category[];
+    constructor () {
+      this.categories = []
+    }
+
+    create ({ name, description }: ICreateCategoryDTO) {
+      this.categories.push(new Category({
+        name,
+        description,
+        createdAt: new Date().toDateString()
+      }))
+    }
+}
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,18 @@
 import { Request, Router } from 'express'
-import { CategoriesRequest, Category } from '../model/Category'
+import { CategoriesRequest } from '../model/Category'
+import { CategoriesRepository } from '../repositories/CategoryRepositories'
 
 export const categoriesRoutes = Router()
 
-const categories:Category[] = []
+const categoriesRepository = new CategoriesRepository()
 
 categoriesRoutes.post('/', (req:Request<{}, {}, CategoriesRequest >, res) => {
   const { name, description } = req.body
 
-  categories.push(new Category({
+  categoriesRepository.create({
     name,
-    description,
-    createdAt: new Date().toDateString()
-  }))
+    description
+  })
 
   return res.status(201).json({
     name
