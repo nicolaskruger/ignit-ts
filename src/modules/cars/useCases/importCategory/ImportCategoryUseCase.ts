494bcd3285142417cddf51f1903e4c1b46dc7e9f
nicolas-kruger <nicolas.nkruger@gmail.com>
2022-03-14 14:58:36 -0300

    conhecendo o conceito de stram

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,8 +1,17 @@
 import { Express } from 'express'
 import fs from 'fs'
+import { parse } from 'csv-parse'
 
 export class ImportCategoryUseCase {
   execute (file: Express.Multer.File) {
-    const data = fs.createReadStream(file.path, 'utf-8')
+    const stream = fs.createReadStream(file.path)
+
+    const parseFile = parse()
+
+    stream.pipe(parseFile)
+
+    parseFile.on('data', async (line) => {
+
+    })
   }
 }
