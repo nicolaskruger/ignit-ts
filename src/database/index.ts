657c3f11442ddb10664426c252f3e40819588c29
nicolas <nicolas.nkruger@gmail.com>
2022-03-18 14:48:35 -0300

    criando container do postgres

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,7 +1,12 @@
 import { DataSource } from 'typeorm'
 
 export const dataSource = new DataSource({
-
+  type: 'postgres',
+  username: 'docker',
+  password: 'ignite',
+  database: 'rentx',
+  host: 'localhost',
+  port: 5432
 })
 
 dataSource.initialize()
