b609bbdb0bd7f0b5771386c0a9d9eeddb3f863c1
nicolas <nicolas.nkruger@gmail.com>
2022-04-06 18:02:51 -0300

    alterando retorno do login

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -3,6 +3,14 @@ import { IUserReposisoty } from '../../repositories/IUserRepository'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 
+export type IResponse = {
+  user: {
+    name: string,
+    email: string
+  },
+  token: string
+}
+
 export class AuthenticateUserUseCase {
     private repository: IUserReposisoty;
 
@@ -10,16 +18,23 @@ export class AuthenticateUserUseCase {
       this.repository = respository
     }
 
-    async execute (login:ILoginDTO) {
+    async execute (login:ILoginDTO): Promise<IResponse> {
       const user = await this.repository.findByEmail(login.email)
       if (!user) { throw new Error('this user not exists') }
       const aprove = bcrypt.compare(login.password, user?.password as string)
       if (!aprove) {
         throw new Error('this password is invalid')
       }
-      const token = jwt.sign({ email: user.email }, 'shhh')
+      const token = jwt.sign({ email: user.email }, '332b7b56acd24bd63d1c0e750e28a80c', {
+        subject: user.id,
+        expiresIn: '1d'
+      })
 
       return {
+        user: {
+          name: user.name,
+          email: user.email
+        },
         token: token
       }
     }
