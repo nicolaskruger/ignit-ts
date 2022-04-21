704e970d7dc0046cea6aac7f8f046cb60985f797
Nicolas Kruger <nicolas.nkruger@gmail.com>
2022-04-21 15:12:04 -0300

    melhorrando documentacao

--- src/routes/account.routes.ts
+++ src/routes/account.routes.ts
@@ -8,10 +8,62 @@ import { CreateUserController } from '../modules/accounts/useCases/createUser/Cr
 const accountRoutes = Router()
 
 accountRoutes.post('/', async (req: Request<{}, {}, ICreateUserDTO>, res: Response) => {
+  /*
+        #swagger.requestBody = {
+              required: true,
+              content: {
+                  "application/json": {
+                      schema: {
+                        "type": "object",
+                        "properties": {
+                          "name": {
+                            "type": "string"
+                          },
+                          "a": {
+                            "type": "string"
+                          },
+                          "userName": {
+                            "type": "string"
+                          },
+                          "password": {
+                            "type": "string"
+                          },
+                          "email": {
+                            "type": "string"
+                          },
+                          "driverLicense": {
+                            "type": "string"
+                          }
+                        }
+                       },
+                  }
+              }
+          }
+  */
   return await container.resolve(CreateUserController).handle(req, res)
 })
 
 accountRoutes.post('/login', async (req: Request<{}, {}, ILoginDTO>, res: Response) => {
+  /*
+        #swagger.requestBody = {
+              required: true,
+              content: {
+                  "application/json": {
+                      schema: {
+                        "type": "object",
+                        "properties": {
+                          "email": {
+                            "type": "string"
+                          },
+                          "password": {
+                            "type": "string"
+                          }
+                        }
+                       },
+                  }
+              }
+          }
+  */
   return await container.resolve(AuthenticateUserController).handle(req, res)
 })
 
