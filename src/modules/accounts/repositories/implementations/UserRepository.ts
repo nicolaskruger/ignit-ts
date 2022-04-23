16e735123e803522bef5a606ff08ce89aee556a4
Nicolas Kruger <nicolas.nkruger@gmail.com>
2022-04-23 16:33:49 -0300

    delete file

--- src/modules/accounts/repositories/implementations/UserRepository.ts
+++ src/modules/accounts/repositories/implementations/UserRepository.ts
@@ -4,8 +4,6 @@ import { IUserReposisoty } from '../IUserRepository'
 import { v4 } from 'uuid'
 import { ICreateUserDTO } from '../../dtos/ICreateUserDTO'
 import { AppError } from '../../../../errors/AppError'
-import { resolve } from 'path'
-import fs from 'fs'
 
 export class UserRepository implements IUserReposisoty {
     private repository:Repository<User>;
@@ -18,11 +16,6 @@ export class UserRepository implements IUserReposisoty {
       const user = await this.repository.findOneBy({ id }) as User
 
       if (!user) throw new AppError('User not found')
-
-      if (user.avatar) {
-        const back = '_'.repeat(5).split('').map(v => '..')
-        fs.unlinkSync(resolve(__dirname, ...back, 'tmp', 'avatar', user.avatar))
-      }
 
       user.avatar = avatar
 
--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,6 +1,8 @@
 import { Express } from 'express'
+import { deleteFile } from '../../../../utils/file'
 import { User } from '../../entities/User'
 import { UserRepository } from '../../repositories/implementations/UserRepository'
+import { resolve } from 'path'
 
 export class UpdateUserAvatarUseCase {
     private repository: UserRepository
@@ -10,6 +12,10 @@ export class UpdateUserAvatarUseCase {
     }
 
     async execute (file: Express.Multer.File, user: User) {
+      if (user.avatar) {
+        const back = '_'.repeat(5).split('').map(v => '..')
+        await deleteFile(resolve(__dirname, ...back, 'tmp', 'avatar', user.avatar))
+      }
       await this.repository.changeAvatar(user.id, file.filename)
       return {
         msg: 'Alter with sucess'
